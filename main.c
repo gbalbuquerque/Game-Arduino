2a3a84298362b5fad70c37e24d03955faa104e7a
Guilherme Barboza de Albuquerque <gui67785@gmail.com>
2024-04-03 06:59:48 -0300

    Vetores e Setup

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
-#include <stdio.h>
+// C++ code
+//
+#include <LiquidCrystal.h>
 
-int main(void){
-    printf("Hello World!");
-    return 0;
+
+LiquidCrystal lcd(12,11,10,9,8,7);
+
+
+String pgtfacil[5]{
+
+
+};
+
+String pgtmed[5]{
+
+
+};
+
+String pgtdif[5]{
+
+
+};
+
+int nivelpgt = 0;
+int acertos = 0;
+int numpgt = 0;
+
+void setup()
+{
+  pinMode(1,INPUT_PULLUP);
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  lcd.begin(16,2);
+  lcd.print("Bem vindo ao jogo!");
+}
+
+void loop()
+{
+  lcd.setCursor(0,1);
+  for (int i = 0; i < 16; i++) {
+    lcd.scrollDisplayLeft();
+    delay(300);
+  }
+  lcd.setCursor(0,1);
+  delay(1000);
 }
