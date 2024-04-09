4c649a4f1beae0081003b93bd665bd5730000f99
Guilherme Barboza de Albuquerque <gui67785@gmail.com>
2024-04-09 15:36:01 -0300

    ATT PERGUNTAS

--- main.c
+++ main.c
@@ -1,49 +1,121 @@
-// C++ code
-//
-#include <LiquidCrystal.h>
+/***************************************************************
 
+				JOGO DE PERGUNTAS NO ARDUINO
+
+CERTIFIQUE-SE DE ANTES DE JOGAR LER A DOCUMENTAÇÃO
+DISPONÍVEL EM: https://github.com/gbalbuquerque/Game-Arduino
+
+***************************************************************/
+#include <LiquidCrystal.h>
+#include <stdlib.h>
 
 LiquidCrystal lcd(12,11,10,9,8,7);
 
+int btsim = 1;
+int btnao = 2;
+int btpular = 3;
+int btca = 4;
+int buzzer = 13;
 
-String pgtfacil[5]{
 
+int maxperguntas = 15;
+int perguntas = 0; 
+int pontuacao = 0;
+int tentativas = 3;
+
+
+
+String pgtfacil[10] = {
+  "  O arduino tem 14 pinos?",
+  "  O cachorro tem 4 patas?",
+  "  A Terra é plana?",
+  "	 O Sol é uma estrela",
+  "	 Os seres humanos precisam de oxigênio para sobreviver?",
+  "  A gravidade é uma força fundamental no universo?",
+  "  A Antártica é o continente mais frio da Terra?",
+  "  A água é composta por hidrogênio e oxigênio?",
+  "O zero é um número ímpar?",
+  "As plantas podem viver sem luz solar?",
+};
+
+
+int rpgtfacil[10] = {1,1,0,1,1,1,1,1,0,0};
+
+
+String pgtmed[5] = {
+  "",
+  "",
+  "",
+  "",
+  ""
 
 };
 
-String pgtmed[5]{
-
+String pgtdif[5] = {
+  "",
+  "",
+  "",
+  "",
+  ""
 
 };
-
-String pgtdif[5]{
-
-
-};
-
-int nivelpgt = 0;
-int acertos = 0;
-int numpgt = 0;
 
 void setup()
 {
-  pinMode(1,INPUT_PULLUP);
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
-  pinMode(4,INPUT_PULLUP);
+  //config
+  pinMode(btsim,INPUT_PULLUP);
+  pinMode(btnao,INPUT_PULLUP);
+  pinMode(btpular,INPUT_PULLUP);
+  pinMode(btca,INPUT_PULLUP);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  //Home
   lcd.begin(16,2);
-  lcd.print("Bem vindo ao jogo!");
+  lcd.setCursor(0, 0);             
+  lcd.print("BEM VINDO ");
+  lcd.setCursor(0, 1);
+  lcd.print("FEIANO!");
+  delay(3000);
+  lcd.clear();
+  lcd.print("JOGO DO QUIZ!");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("APERTE O BT 1");
+  lcd.setCursor(0,1);
+  lcd.print(" PARA COMECAR");
+  
+  while (digitalRead(btca) == HIGH) {
+  }
+  loop();
 }
+
+void pgtcerta(){
+  lcd.setCursor(0,0);
+  lcd.print("Correta!");
+    
+}
+
 
 void loop()
 {
-  lcd.setCursor(0,1);
+  btsim = digitalRead(1);
+  btnao = digitalRead(2);
+  btpular = digitalRead(3);
+  btca = digitalRead(4);
+  
+  if(perguntas != maxperguntas){
+    
+  }
+  
+  /*lcd.setCursor(0,1);
   for (int i = 0; i < 16; i++) {
     lcd.scrollDisplayLeft();
     delay(300);
   }
   lcd.setCursor(0,1);
   delay(1000);
+  lcd.clear();*/
+  
 }
