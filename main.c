ba56ad87ff4976550b52d74c32079d74179c1884
Guilherme Barboza de Albuquerque <gui67785@gmail.com>
2024-04-10 06:53:45 -0300

    Led

--- main.c
+++ main.c
@@ -1,121 +1,166 @@
-/***************************************************************
-
-				JOGO DE PERGUNTAS NO ARDUINO
-
-CERTIFIQUE-SE DE ANTES DE JOGAR LER A DOCUMENTAÇÃO
-DISPONÍVEL EM: https://github.com/gbalbuquerque/Game-Arduino
-
-***************************************************************/
 #include <LiquidCrystal.h>
 #include <stdlib.h>
 
-LiquidCrystal lcd(12,11,10,9,8,7);
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
-int btsim = 1;
+//botoes, buzzer e led
+int btsim = A0;
 int btnao = 2;
 int btpular = 3;
 int btca = 4;
 int buzzer = 13;
+int lv = 5;
+int lred = 6;  
 
-
-int maxperguntas = 15;
-int perguntas = 0; 
+//variaveis 
+int maxperguntasf = 5;
+int perguntas = 0;
 int pontuacao = 0;
 int tentativas = 3;
-
-
+int dificuldade = 0;
+int numQ = 0;
+bool mostrandoPergunta = false;
+int indiceAleatorio;
 
 String pgtfacil[10] = {
-  "  O arduino tem 14 pinos?",
-  "  O cachorro tem 4 patas?",
-  "  A Terra é plana?",
-  "	 O Sol é uma estrela",
-  "	 Os seres humanos precisam de oxigênio para sobreviver?",
-  "  A gravidade é uma força fundamental no universo?",
-  "  A Antártica é o continente mais frio da Terra?",
-  "  A água é composta por hidrogênio e oxigênio?",
-  "O zero é um número ímpar?",
-  "As plantas podem viver sem luz solar?",
+    "  O arduino tem 14 pinos?",
+    "  O cachorro tem 4 patas?",
+    "  A Terra eh plana?",
+    "  O Sol eh uma estrela",
+    "  Os seres humanos precisam de oxigenio para sobreviver?",
+    "  A gravidade eh uma forca fundamental no universo?",
+    "  A Antartica eh o continente mais frio da Terra?",
+    "  A agua eh composta por hidrogenio e oxigenio?",
+    "  O zero é um numero par?",
+    "  As plantas podem viver sem luz solar?",
 };
 
 
-int rpgtfacil[10] = {1,1,0,1,1,1,1,1,0,0};
 
+String pgtfinal[1] = { 
+  "Voce gostou do Jogo?" 
+}; 
+ 
 
-String pgtmed[5] = {
-  "",
-  "",
-  "",
-  "",
-  ""
-
-};
-
-String pgtdif[5] = {
-  "",
-  "",
-  "",
-  "",
-  ""
-
-};
+bool rpgtfacil[10] = {true, true, false, true, true, true, true, true, false, false};
+bool rpgtmedio[10] = {true, false, true, true, true,true, true, true, false, false};
+bool rpgtdificil[10] = {true, true, false, false, true};
+bool rpgtfinal[1] = {true};
 
 void setup()
 {
-  //config
-  pinMode(btsim,INPUT_PULLUP);
-  pinMode(btnao,INPUT_PULLUP);
-  pinMode(btpular,INPUT_PULLUP);
-  pinMode(btca,INPUT_PULLUP);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(buzzer,OUTPUT);
-  //Home
-  lcd.begin(16,2);
-  lcd.setCursor(0, 0);             
-  lcd.print("BEM VINDO ");
-  lcd.setCursor(0, 1);
-  lcd.print("FEIANO!");
-  delay(3000);
-  lcd.clear();
-  lcd.print("JOGO DO QUIZ!");
-  delay(3000);
-  lcd.clear();
-  lcd.setCursor(1,0);
-  lcd.print("APERTE O BT 1");
-  lcd.setCursor(0,1);
-  lcd.print(" PARA COMECAR");
-  
-  while (digitalRead(btca) == HIGH) {
-  }
-  loop();
+    //config
+    Serial.begin(9600);
+    pinMode(btsim, INPUT_PULLUP);
+    pinMode(btnao, INPUT_PULLUP);
+    pinMode(btpular, INPUT_PULLUP);
+    pinMode(btca, INPUT_PULLUP);
+    pinMode(lred, OUTPUT);
+    pinMode(lv, OUTPUT);
+    pinMode(buzzer, OUTPUT);
+    randomSeed(analogRead(0));
+
+    //home
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("BEM VINDO!");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("APERTE O BT 1");
+    lcd.setCursor(0, 1);
+    lcd.print(" PARA COMECAR! ");
+
+    //loop
+    while (digitalRead(btca) == HIGH)
+    {
+    }
+    lcd.clear();
+    loop();
 }
 
-void pgtcerta(){
-  lcd.setCursor(0,0);
-  lcd.print("Correta!");
-    
+
+
+void exibirPontuacao() {
+    lcd.setCursor(0, 1);
+    lcd.print("Pontuacao: ");
+    lcd.print(pontuacao);
+}
+
+void exibirQuestao() {
+  	lcd.print("Questao:");
+  	lcd.print(numQ);
+}
+
+void verifica_resposta(bool resposta) {
+  bool resposta_correta = false;
+  if (dificuldade < 3) {
+    switch (dificuldade) {
+      case 0: resposta_correta = rpgtfacil[indiceAleatorio]; 
+              break;
+      case 1: resposta_correta = rpgtmedio[indiceAleatorio]; 
+              break;
+      case 2: resposta_correta = rpgtdificil[indiceAleatorio]; 
+              break;
+    }
+
+    if (resposta == resposta_correta) {
+      lcd.clear();
+      lcd.print("Correta!");
+      digitalWrite(lv, HIGH); 
+      delay(3000);
+      digitalWrite(lv, LOW); 
+      pontuacao++;
+    } else {
+      lcd.clear();
+      lcd.print("Incorreta!");
+      digitalWrite(lred, HIGH); 
+      delay(3000);
+      digitalWrite(lred, LOW);
+    }
+    delay(3000);
+    exibirPontuacao();
+    delay(5000);
+  }
 }
 
 
 void loop()
 {
-  btsim = digitalRead(1);
-  btnao = digitalRead(2);
-  btpular = digitalRead(3);
-  btca = digitalRead(4);
-  
-  if(perguntas != maxperguntas){
-    
-  }
-  
-  /*lcd.setCursor(0,1);
-  for (int i = 0; i < 16; i++) {
-    lcd.scrollDisplayLeft();
-    delay(300);
-  }
-  lcd.setCursor(0,1);
-  delay(1000);
-  lcd.clear();*/
-  
+    if (perguntas < maxperguntasf)
+    {
+        if (!mostrandoPergunta)
+        {            
+            indiceAleatorio = random(10); 
+            lcd.clear();
+            lcd.setCursor(1, 0);
+            numQ++;
+            exibirQuestao();
+            delay(2000);
+            lcd.clear();
+            lcd.setCursor(1, 0);
+            lcd.print(pgtfacil[indiceAleatorio]);
+            for (int i = 0; i < 16; i++) {      
+                lcd.scrollDisplayLeft();
+                delay(300);
+            }
+            mostrandoPergunta = true;
+        }
+        
+        if (digitalRead(btsim) == LOW)
+        {
+            verifica_resposta(true); 
+            mostrandoPergunta = false;
+            perguntas++;
+        }
+
+        if (digitalRead(btnao) == LOW)
+        {
+            verifica_resposta(false); 
+            mostrandoPergunta = false;
+            perguntas++;
+        }
+
+        delay(100);
+    }
 }
